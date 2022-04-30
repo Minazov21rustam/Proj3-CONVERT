8717ec54a68ee303f9d83b0f931d8074d6c0697e
Skywalker <you@example.com>
2022-04-30 17:44:43 +0400

    ForCheck

---
+++ 3242/Controller/controller.mjs
@@ -0,0 +1,82 @@
+export default class Controller {
+  constructor(view, model) {
+    this.view = view;
+    this.model = model;
+
+    this.view.onLeftCurrencyChange = this.onLeftCurrencyChange;
+    this.view.onRigthCurrencyChange = this.onRigthCurrencyChange;
+
+    this.view.leftInp.onkeyup = (e) => {
+      this.model.setLeftValue(e.target.value);
+      this.setRightInp();
+    };
+
+    this.view.rightInp.onkeyup = (e) => {
+      this.model.setRightValue(e.target.value);
+      this.setLeftInp();
+    };
+
+    this.init();
+  }
+
+  init() {
+    this.getAndSetCurrency();
+  }
+
+  setRightInp() {
+    this.view.rightInp.value = this.model.leftValue * this.model.coefficient;
+  //   console.log(this.view.rightInp.value, this.model.leftValue, this.model.coefficient);
+  }
+  setLeftInp() {
+    this.view.leftInp.value = this.model.rightValue * this.model.coefficient;
+    console.log(this.view.leftInp.value, this.model.rightValue, this.model.coefficient);
+  }
+
+  onLeftCurrencyChange = async (currency) => {
+    // console.log(currency);
+    this.model.setLeftCurrency(currency);
+    await this.getAndSetCurrency();
+    this.setRightInp();
+    this.setLeftInp();
+
+    this.writeRightCurency();
+    this.writeLeftCurency();
+  };
+
+  onRigthCurrencyChange = async (currency) => {
+    this.model.setRightCurrency(currency);
+    await this.getAndSetCurrency();
+    this.writeRightCurency();
+    this.writeLeftCurency();
+
+    this.setRightInp();
+    this.setLeftInp();
+  };
+
+  fetchCurency = async (base, symbols) => {
+    const response = await fetch(
+      `https://api.exchangerate.host/latest?base=${base}&symbols=${symbols}`
+    );
+    const data = await response.json();
+    // console.log(base, symbols);
+    return data;
+  };
+  getAndSetCurrency = async () => {
+    const data = await this.fetchCurency(
+      this.model.leftCurrency,
+      this.model.rightCurrency
+    );
+    this.model.setCoefficent(data.rates[this.model.rightCurrency]);
+    // console.log(data.rates[this.model.rightCurrency]);
+  };
+
+  writeLeftCurency() {
+    const curLeftDiv = document.querySelector(".curency-Left");
+    curLeftDiv.innerText = `1 ${this.model.leftCurrency} = ${(this.model.coefficient).toFixed(3)} ${this.model.rightCurrency}`;
+  }
+
+  writeRightCurency() {
+    const curRightDiv = document.querySelector(".curency-Right");
+    curRightDiv.innerText = `1 ${this.model.rightCurrency} = ${(1/this.model.coefficient).toFixed(3)} ${this.model.leftCurrency}`;
+  }
+}
---
+++ 3242/View/view.mjs
@@ -0,0 +1,63 @@
+export default class View {
+  constructor() {
+    // this.root = document.getElementById("root");
+    this.onLeftCurrencyChange = () => {};
+    this.onRigthCurrencyChange = () => {};
+    this.leftInp = document.getElementById("leftCurInp");
+    this.rightInp = document.getElementById("rigthCurInp");
+
+    this.init();
+  }
+
+  init() {
+    // const allRadio = document.querySelectorAll('input')
+    const radio1 = document.querySelector("#radio-1");
+    radio1.addEventListener("click", (e) => {
+      e.preventDefault();
+      this.onLeftCurrencyChange("RUB");
+      console.log(this.leftInp.value)
+    });
+    const radio2 = document.querySelector("#radio-2");
+    radio2.addEventListener("click", (e) => {
+      e.preventDefault();
+      this.onLeftCurrencyChange("USD");
+    });
+    const radio3 = document.querySelector("#radio-3");
+    radio3.addEventListener("click", (e) => {
+      e.preventDefault();
+      this.onLeftCurrencyChange("EUR");
+    });
+    const radio4 = document.querySelector("#radio-4");
+    radio4.addEventListener("click", (e) => {
+      e.preventDefault();
+      this.onLeftCurrencyChange("GBP");
+    });
+    // <===================================Right==================================>
+
+    const radio5 = document.querySelector("#radio-5");
+    radio5.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      this.onRigthCurrencyChange("RUB");
+    });
+
+    const radio6 = document.querySelector("#radio-6");
+    radio6.addEventListener("click", (e) => {
+      e.preventDefault();
+      this.onRigthCurrencyChange("USD");
+    });
+
+    const radio7 = document.querySelector("#radio-7");
+    radio7.addEventListener("click", (e) => {
+      e.preventDefault();
+      this.onRigthCurrencyChange("EUR");
+    });
+
+    const radio8 = document.querySelector("#radio-8");
+    radio8.addEventListener("click", (e) => {
+      e.preventDefault();
+      this.onRigthCurrencyChange("GBP");
+    });
+	
+  }
+}
