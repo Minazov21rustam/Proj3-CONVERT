9e1ef6b2398578af3222fb322dd96880ca330570
Skywalker <you@example.com>
2022-05-20 13:43:26 +0400

    YEEEEEES!!!

--- ConvertApp/View/view.mjs
+++ ConvertApp/View/view.mjs
@@ -11,13 +11,5 @@ export default class View {
   init() {
     this.leftButtons = document.querySelectorAll('.left-Btn')
     this.rightButtons = document.querySelectorAll('.right-Btn')
-    // this.radio1 = document.querySelector("#radio-1");
-    // this.radio2 = document.querySelector("#radio-2");
-    // this.radio3 = document.querySelector("#radio-3");
-    // this.radio4 = document.querySelector("#radio-4");
-    // this.radio5 = document.querySelector("#radio-5");
-    // this.radio6 = document.querySelector("#radio-6");
-    // this.radio7 = document.querySelector("#radio-7");
-    // this.radio8 = document.querySelector("#radio-8");
   }
 }
